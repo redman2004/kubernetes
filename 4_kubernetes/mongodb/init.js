52896c9fee905357be95f0fa55d0174f408e5fc8
Tieoule <tieoule.coulibaly@epita.fr>
2026-03-29 15:26:44 +0200

    fix(): remaining issues

--- 4_kubernetes/mongodb/init.js
+++ 4_kubernetes/mongodb/init.js
@@ -1,5 +1,9 @@
-db.getSiblingDB(process.env.SUNDALAND_MONGODB_DATABASE).createUser({
+const db = db.getSiblingDB(process.env.SUNDALAND_MONGODB_DATABASE);
+
+db.createUser({
     user: process.env.SUNDALAND_MONGODB_USER,
     pwd: process.env.SUNDALAND_MONGODB_PASSWORD,
     roles: [{ role: "readWrite", db: process.env.SUNDALAND_MONGODB_DATABASE }]
 });
+
+db.createCollection("products");
