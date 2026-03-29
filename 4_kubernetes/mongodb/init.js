348c55cadbcaef4302baf79445310ab2c0d21344
Tieoule <tieoule.coulibaly@epita.fr>
2026-03-29 14:51:55 +0200

    feat(): kubernetes

---
+++ 4_kubernetes/mongodb/init.js
@@ -0,0 +1,6 @@
+// FIXME
+db.getSiblingDB(process.env.SUNDALAND_MONGODB_DATABASE).createUser({
+    user: process.env.SUNDALAND_MONGODB_USER,
+    pwd: process.env.SUNDALAND_MONGODB_PASSWORD,
+    roles: [{ role: "readWrite", db: process.env.SUNDALAND_MONGODB_DATABASE }]
+});
